cc2c36b538dc683210ba1a14609b9ae81d33221d
Panurb <panu-sakari@hotmail.com>
2025-11-18 21:09:23 +0200

    Move mesh creation to render_mesh.

---
+++ threedee/include/render_mesh.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "render.h"
+
+
+MeshData create_mesh_triangle(void);
+
+MeshData create_mesh_triangle_2d(void);
+
+MeshData create_mesh_quad(void);
+
+MeshData create_mesh_line(void);
+
+MeshData create_mesh_text(TTF_GPUAtlasDrawSequence data);
