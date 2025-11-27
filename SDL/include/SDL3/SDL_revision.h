fc7bec43ceff0b3d56e27aae693e83cff16cf540
Panurb <panu-sakari@hotmail.com>
2025-11-28 00:38:26 +0200

    Update SDL to version 3.3.2.

--- SDL/include/SDL3/SDL_revision.h
+++ SDL/include/SDL3/SDL_revision.h
@@ -48,9 +48,9 @@
  */
 #define SDL_REVISION "Some arbitrary string decided at SDL build time"
 #elif defined(SDL_VENDOR_INFO)
-#define SDL_REVISION "release-3.2.14-0-g8d604353a (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "preview-3.3.2-0-gb31c4b70b (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "release-3.2.14-0-g8d604353a"
+#define SDL_REVISION "preview-3.3.2-0-gb31c4b70b"
 #endif
 
 #endif /* SDL_revision_h_ */
